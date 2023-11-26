a579899ca301ab40bf6206d30825b32b182abc09
midulislamrasel <midulraddy121@gmail.com>
2023-11-26 19:32:17 +0600

    add

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -2,7 +2,6 @@ import OfferNow from "../components/offerNow/OfferNow";
 import Hero from "../components/hero/Hero";
 import NewArrival from "../components/newArrival/NewArrival";
 import OurStory from "../components/ourStory/OurStory";
-import Customer from "../(customer)/account/page";
 
 export default function Home() {
   return (
@@ -11,7 +10,6 @@ export default function Home() {
       <NewArrival />
       <OurStory />
       <OfferNow />
-      <Customer />
     </>
   );
 }
