cf259debcc5df0f7386821dd76b4489e34c70e86
rayhanwd <rayhanbd4400@gmail.com>
2023-12-04 21:52:34 +0600

    u

---
+++ src/app/(merchant)/layout.js
@@ -0,0 +1,13 @@
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+
+export const metadata = {
+  title: "Kazi food | Restaurant",
+  description:
+    "Kazifood is an online food and grocery delivery platform owned by Berlin-based Delivery Hero",
+  icons: [{ rel: "icon", url: "favicon.ico" }],
+};
+
+export default function RestaurantLayout({ children }) {
+  return <>{children}</>;
+}
