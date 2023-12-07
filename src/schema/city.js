a0da6bc0dc37fbe2e03fbf5976b1fd7d0d4cf862
midulislamrasel <midulraddy121@gmail.com>
2023-12-07 21:33:10 +0600

    add

--- src/schema/city.js
+++ src/schema/city.js
@@ -1,27 +1,20 @@
 const mongoose = require("mongoose");
 
-// Define the schema
-const photoSchema = new mongoose.Schema({
+const photoSchema = new Schema({
   cname: {
     type: String,
     required: true,
-    unique: true,
-    // You can add additional validation rules for CNAME if needed
   },
   cover: {
     type: String,
     required: true,
-    // You can add validation rules for cover images (e.g., URL validation)
   },
   thumbnail: {
     type: String,
     required: true,
-    // You can add validation rules for thumbnail images (e.g., URL validation)
   },
 });
 
-// Create the Photo model
 const Photo = mongoose.model("Photo", photoSchema);
 
-// Export the model
 module.exports = Photo;
